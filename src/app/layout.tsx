c5497d1cad88f1ee6f27064e573c598328845722
ysrkaanky <ysrkaanky@gmail.com>
2024-03-21 18:37:13 +0300

    favicon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next'
 import './globals.css'
 import { Footer, Header } from '@/components/Layout'
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
 
 export default function RootLayout({
   children,
